b7305f83d00a06a9c5d45887f111ea3e04779704
Travis CI <travis@travis-ci.org>
2017-12-17 19:45:45 -0800

    (ci) after_deploy generate doc/ghpages

---
+++ docs/build.sh
@@ -0,0 +1,10 @@
+#/usr/local/env sh
+
+(cd docs && go run main.go &&
+	sed -i 's|lab.md|index.md|' *.md &&
+	mv lab.md index.md)
+if [ -n ${DEPLOY} ]; then
+	git config --global user.email "travis@travis-ci.org" && git config --global user.name "Travis CI"
+	git remote add origin-lab https://${GITHUB_TOKEN}@github.com/zaquestion/lab.git > /dev/null 2>&1
+	git checkout docstest && git add docs && git commit -m "(docs) ${TRAVIS_TAG}" && git push origin-lab master
+fi
