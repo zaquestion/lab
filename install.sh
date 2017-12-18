fe20daf83baa683d352ea248239c3ef431c93568
Zaq? Wiedmann <zaquestion@gmail.com>
2017-12-18 04:43:18 -0800

    (fix) install script needs to pull and cd to project first, .gitignore vendors

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 set -e
 [[ -z $DEBUG ]] || set -x
 
-go get -u github.com/golang/dep/cmd/dep
-dep ensure
-go install -ldflags "-X \"main.version=$(git  rev-parse --short=10 HEAD)\""  github.com/zaquestion/lab
+
+
+(go get -u -d github.com/zaquestion/lab &&
+cd $GOPATH/src/github.com/zaquestion/lab &&
+go get -u github.com/golang/dep/cmd/dep &&
+dep ensure &&
+go install -ldflags "-X \"main.version=$(git  rev-parse --short=10 HEAD)\""  github.com/zaquestion/lab)
