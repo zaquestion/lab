14853a0bede2844ee46473b5a03e0a0c08314b51
Zaq? Wiedmann <zaquestion@gmail.com>
2017-12-17 20:14:13 -0800

    (fix,ci) generate docs before_deploy and checkout correct branch

--- docs/build.sh
+++ docs/build.sh
@@ -6,5 +6,5 @@
 if [ -n ${DEPLOY} ]; then
 	git config --global user.email "travis@travis-ci.org" && git config --global user.name "Travis CI"
 	git remote add origin-lab https://${GITHUB_TOKEN}@github.com/zaquestion/lab.git > /dev/null 2>&1
-	git checkout docstest && git add docs && git commit -m "(docs) ${TRAVIS_TAG}" && git push origin-lab master
+	git checkout master && git add docs && git commit -m "(docs) ${TRAVIS_TAG}" && git push origin-lab master
 fi
