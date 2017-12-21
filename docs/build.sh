404fefe307802b0aeb82b2048124563c417a3b24
Zaq? Wiedmann <zaquestion@gmail.com>
2017-12-21 11:34:50 -0800

    (ci) set -x on doc builder and fetch remote first

--- docs/build.sh
+++ docs/build.sh
@@ -1,10 +1,12 @@
 #/usr/local/env sh
 
+set -x
+
 (cd docs && go run main.go &&
 	sed -i 's|lab.md|index.md|' *.md &&
 	mv lab.md index.md)
 if [ ! -z ${DEPLOY} ]; then
 	git config --global user.email "travis@travis-ci.org" && git config --global user.name "Travis CI"
 	git remote add origin-lab https://${GITHUB_TOKEN}@github.com/zaquestion/lab.git > /dev/null 2>&1
-	git checkout master && git add docs && git commit -m "(docs) ${TRAVIS_TAG}" && git push origin-lab master
+	git fetch origin-lab && git checkout master && git add docs && git commit -m "(docs) ${TRAVIS_TAG}" && git push origin-lab master
 fi
