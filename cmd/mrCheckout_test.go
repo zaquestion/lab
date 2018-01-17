c9a577338fca0b293923923f2f2a47088871ae60
Adam Ryman <adamryman@gmail.com>
2018-01-17 10:58:32 -0800

    Test lab mr checkout -b

--- cmd/mrCheckout_test.go
+++ cmd/mrCheckout_test.go
@@ -39,3 +39,36 @@ func Test_mrCheckoutCmdRun(t *testing.T) {
 	require.Contains(t, eLog, "Test file for MR test")
 	require.Contains(t, eLog, "54fd49a2ac60aeeef5ddc75efecd49f85f7ba9b0")
 }
+
+func Test_mrCheckoutCmdRunWithDifferentName(t *testing.T) {
+	t.Parallel()
+	repo := copyTestRepo(t)
+
+	cmd := exec.Command("../lab_bin", "mr", "checkout", "1", "-b", "mrtest_custom_name")
+	cmd.Dir = repo
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Log(string(b))
+		t.Fatal(err)
+	}
+	t.Log(string(b))
+
+	cmd = exec.Command("git", "branch")
+	cmd.Dir = repo
+
+	branch, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.Contains(t, string(branch), "mrtest_custom_name")
+
+	cmd = exec.Command("git", "log", "-n1")
+	cmd.Dir = repo
+	log, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	eLog := string(log)
+	require.Contains(t, eLog, "Test file for MR test")
+	require.Contains(t, eLog, "54fd49a2ac60aeeef5ddc75efecd49f85f7ba9b0")
+}
