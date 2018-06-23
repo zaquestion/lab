d348fdc69b6d97b7882075c73af5f37b7a8f8516
Zaq? Wiedmann <zaquestion@gmail.com>
2018-06-23 01:51:25 -0700

    [#163] block hub passthrough for `lab browse` to avoid confusion

--- cmd/root.go
+++ cmd/root.go
@@ -197,6 +197,9 @@ func Execute() {
 		if os.Args[1] == "create" {
 			log.Fatalf("Please call `hub create` directly for github, the lab equivalent is `lab project create`")
 		}
+		if os.Args[1] == "browse" {
+			log.Fatalf("Please call `hub browse` directly for github, the lab equivalent is `lab <object> browse`")
+		}
 
 		// Passthrough to git for any unrecognized commands
 		err = git.New(os.Args[1:]...).Run()
