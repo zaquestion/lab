62e327719753b25ffd1a42a3c3a2136f65b3ebbf
Nick Polanco <npolanco@mobiquityinc.com>
2018-07-03 11:04:12 +0200

    feat: config per page flag on

--- cmd/mr_list_test.go
+++ cmd/mr_list_test.go
@@ -37,7 +37,7 @@ func Test_mrListFlagLabel(t *testing.T) {
 
 	mrs := strings.Split(string(b), "\n")
 	t.Log(mrs)
-	require.Equal(t, "#3 for testings filtering with labels and lists", mrs[0])
+	require.Equal(t, "#3 Test filtering by label", mrs[0])
 }
 
 func Test_mrListStateMerged(t *testing.T) {
@@ -70,4 +70,20 @@ func Test_mrListStateClosed(t *testing.T) {
 	mrs := strings.Split(string(b), "\n")
 	t.Log(mrs)
 	require.Equal(t, "#5 closed mr", mrs[0])
+}
+
+func Test_mrListFivePerPage(t *testing.T) {
+	t.Parallel()
+	repo := copyTestRepo(t)
+	cmd := exec.Command("../lab_bin", "mr", "list", "-n", 5)
+	cmd.Dir = repo
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mrs := strings.Split(string(b), "\n")
+	t.Log(mrs)
+	require.Equal(t, "#6 with per page preset", mrs[0])
 }
