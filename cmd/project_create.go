44baecde73041f9c64efc795cf60f8c56b6f137e
Clayton Carter <claytonrcarter@gmail.com>
2018-11-16 14:44:54 -0500

    project create: update help text

--- cmd/project_create.go
+++ cmd/project_create.go
@@ -11,13 +11,24 @@ import (
 	lab "github.com/zaquestion/lab/internal/gitlab"
 )
 
+// private and public are defined in snippet_create.go
+var internal bool
+
 // projectCreateCmd represents the create command
 var projectCreateCmd = &cobra.Command{
 	Use:   "create [path]",
 	Short: "Create a new project on GitLab",
-	Long: `If no path or name is provided the name of the git repo working directory
+	Long: `Create a new project on GitLab in your user namespace.
 
-path refers the path on gitlab including the full group/namespace/project. If no path or name is provided and the directory is a git repo the name of the current working directory will be used.`,
+path refers to the path on GitLab not including the group/namespace. If no path
+or name is provided and the current directory is a git repo, the name of the
+current working directory will be used.`,
+	Example: `# this command...                          # creates this project
+lab project create                         # user/<curr dir> named <curr dir>
+                                           # (above only works w/i git repo)
+lab project create myproject               # user/myproject named myproject
+lab project create myproject -n "new proj" # user/myproject named "new proj"
+lab project create -n "new proj"           # user/new-proj named "new proj"`,
 	Args: cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
@@ -29,10 +40,17 @@ path refers the path on gitlab including the full group/namespace/project. If no
 			log.Fatal("path or name must be set")
 		}
 
+		// set the default visibility
 		visibility := gitlab.InternalVisibility
+
+		// now override the visibility if the user passed in relevant flags. if
+		// the user passes multiple flags, this will use the "most private"
+		// option given, ignoring the rest
 		switch {
 		case private:
 			visibility = gitlab.PrivateVisibility
+		case internal:
+			visibility = gitlab.InternalVisibility
 		case public:
 			visibility = gitlab.PublicVisibility
 		}
@@ -75,9 +93,10 @@ func determinePath(args []string, name string) string {
 }
 
 func init() {
-	projectCreateCmd.Flags().StringP("name", "n", "", "name to use for the new project")
-	projectCreateCmd.Flags().StringP("description", "d", "", "description to use for the new project")
-	projectCreateCmd.Flags().BoolVarP(&private, "private", "p", false, "Make project private; visible only to project members (default: internal)")
-	projectCreateCmd.Flags().BoolVar(&public, "public", false, "Make project public; can be accessed without any authentication (default: internal)")
+	projectCreateCmd.Flags().StringP("name", "n", "", "name of the new project")
+	projectCreateCmd.Flags().StringP("description", "d", "", "description of the new project")
+	projectCreateCmd.Flags().BoolVarP(&private, "private", "p", false, "Make project private: visible only to project members")
+	projectCreateCmd.Flags().BoolVar(&public, "public", false, "Make project public: visible without any authentication")
+	projectCreateCmd.Flags().BoolVar(&internal,"internal", false, "Make project internal: visible to any authenticated user (default)")
 	projectCmd.AddCommand(projectCreateCmd)
 }
