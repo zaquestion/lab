fe56bb19bfde3dae61180871b1bfa13cedb7cf98
rsteube <rsteube@users.noreply.github.com>
2018-11-27 15:47:30 +0100

    enabled spaces in description

--- cmd/completion_function.go
+++ cmd/completion_function.go
@@ -7,26 +7,26 @@ function __lab_completion_project {
 }
 
 function __lab_completion_remote_branches {
-  # TODO enable spaces in description
-  branches=($(git branch -r -v | grep -v 'origin/HEAD' | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/  origin\/([^ ]*)[ ]*(.*)/\1:\2/' -e 's/ /_/g'))
+  local IFS=$'\n'
+  branches=($(git branch -r -v | grep -v 'origin/HEAD' | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/  origin\/([^ ]*)[ ]*(.*)/\1:\2/'))
   [ -z "$branches" ] || _describe 'branch' branches
 }
 
 function __lab_completion_issue {
-  # TODO enable spaces in description
-  issues=($(lab issue list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/' -e 's/ /_/g'))
+  local IFS=$'\n'
+  issues=($(lab issue list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/'))
   [ -z "$issues" ] || _describe 'issue' issues
 }
 
 function __lab_completion_merge_request {
-  # TODO enable spaces in description
-  merge_requests=($(lab mr list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/' -e 's/ /_/g'))
+  local IFS=$'\n'
+  merge_requests=($(lab mr list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/'))
   [ -z "$merge_requests" ] || _describe 'merge request' merge_requests
 }
 
 function __lab_completion_snippet {
-  # TODO enable spaces in description
-  snippets=($(lab snippet list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/' -e 's/ /_/g'))
+  local IFS=$'\n'
+  snippets=($(lab snippet list | sed -r -e 's/:/\\:/g' -e "s/'//g" -e 's/#([0-9]*) (.*)/\1:\2/'))
   [ -z "$snippets" ] || _describe 'snippets' snippets
 }
 `
