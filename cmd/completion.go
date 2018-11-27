3b1e4b0a2b8317c84b621c0100f2be54a73bc31b
rsteube <rsteube@users.noreply.github.com>
2018-11-27 12:56:01 +0100

    use cobra fork in dep

--- cmd/completion.go
+++ cmd/completion.go
@@ -17,6 +17,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+		//RootCmd.GenBashCompletion(os.Stdout)
 		RootCmd.GenZshCompletion(os.Stdout)
 	},
 }
