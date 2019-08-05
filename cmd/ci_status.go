f91def563196d8d284aaa8c86ac3bed4ca6bdacd
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-08-05 11:33:42 +0100

    Useful summary information

--- cmd/ci_status.go
+++ cmd/ci_status.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"github.com/zaquestion/lab/internal/git"
+        gitlab "github.com/xanzy/go-gitlab"
 	lab "github.com/zaquestion/lab/internal/gitlab"
 )
 
@@ -19,6 +20,8 @@ var (
   useColor bool
   noSkipped bool
   wait bool
+  noCreated bool
+  summaryOnly bool
   jobFormat = "%s:\t%s\t-\t%s\tid: %d\n"
   failed = color.New(color.FgRed)
   passed = color.New(color.FgGreen)
@@ -37,72 +40,136 @@ var ciStatusCmd = &cobra.Command{
 	Example: `lab ci status
 lab ci status --wait`,
 	RunE: nil,
-	Run: func(cmd *cobra.Command, args []string) {
-		branch, err := git.CurrentBranch()
-		if err != nil {
-			log.Fatal(err)
-		}
+	Run: runCommand,
+      }
 
-		if len(args) > 1 {
-			branch = args[1]
-		}
-		remote := determineSourceRemote(branch)
-		if len(args) > 0 {
-			ok, err := git.IsRemote(args[0])
-			if err != nil || !ok {
-				log.Fatal(args[0], " is not a remote:", err)
-			}
-			remote = args[0]
-		}
-		rn, err := git.PathWithNameSpace(remote)
-		if err != nil {
-			log.Fatal(err)
-		}
-		pid := rn
+func runCommand(cmd *cobra.Command, args []string) {
+        branch, err := git.CurrentBranch()
+        if err != nil {
+                log.Fatal(err)
+        }
 
-		w := tabwriter.NewWriter(os.Stdout, 2, 4, 1, byte(' '), 0)
-		jobs, err := lab.CIJobs(pid, branch)
-		if err != nil {
-			log.Fatal(errors.Wrap(err, "failed to find ci jobs"))
-		}
-		jobs = latestJobs(jobs)
+        if len(args) > 1 {
+                branch = args[1]
+        }
+        remote := determineSourceRemote(branch)
+        if len(args) > 0 {
+                ok, err := git.IsRemote(args[0])
+                if err != nil || !ok {
+                        log.Fatal(args[0], " is not a remote:", err)
+                }
+                remote = args[0]
+        }
+        rn, err := git.PathWithNameSpace(remote)
+        if err != nil {
+                log.Fatal(err)
+        }
+        pid := rn
 
-		if len(jobs) == 0 {
-			return
-		}
+        w := tabwriter.NewWriter(os.Stdout, 2, 4, 1, byte(' '), 0)
+        jobs, err := lab.CIJobs(pid, branch)
+        if err != nil {
+                log.Fatal(errors.Wrap(err, "failed to find ci jobs"))
+        }
+        jobs = latestJobs(jobs)
 
-		fmt.Fprintln(w, "Stage:\tName\t-\tStatus")
-		color.NoColor = !useColor
-		var (
-			printer *color.Color
-		)
-		for {
-			for _, job := range jobs {
-				if noSkipped && job.Status == "skipped" {
-					continue
-				} else if onlyFailures && job.Status != "failed" {
-					continue
-				} else {
-                                        printer = statusColor(job.Status)
-					printer.Fprintf(w, jobFormat, job.Stage, job.Name, job.Status, job.ID)
-				}
-			}
-			if !wait {
-				break
-			}
-			if jobs[0].Pipeline.Status != "pending" &&
-				jobs[0].Pipeline.Status != "running" {
-				break
-			}
-			fmt.Fprintln(w)
-		}
+        if len(jobs) == 0 {
+                return
+        }
 
-		fmt.Fprintf(w, "\nPipeline Status: %s\n", jobs[0].Pipeline.Status)
-		if wait && jobs[0].Pipeline.Status != "success" {
-			os.Exit(1)
-		}
-		w.Flush()
-	},
+        if !summaryOnly {
+          fmt.Fprintln(w, "Stage:\tName\t-\tStatus")
+        }
+        color.NoColor = !useColor
+        var (
+                printer *color.Color
+        )
+        pipelineId := jobs[0].Pipeline.ID
+        pipeline, _, err := lab.Client().Pipelines.GetPipeline(pid, pipelineId)
+        if err != nil {
+                log.Fatal(errors.Wrap(err, "failed to get pipeline information"))
+        }
+        for {
+                if !summaryOnly {
+                  for _, job := range jobs {
+                            if noSkipped && job.Status == "skipped" {
+                                    continue
+                            } else if onlyFailures && job.Status != "failed" {
+                                    continue
+                            } else if noCreated && job.Status == "created" {
+                                    continue
+                            } else {
+                                    printer = statusColor(job.Status)
+                                    printer.Fprintf(w, jobFormat, job.Stage, job.Name, job.Status, job.ID)
+                            }
+                  }
+                }
+                if !wait {
+                        break
+                }
+                pl, _, err := lab.Client().Pipelines.GetPipeline(pid, pipelineId)
+                if err != nil {
+                        log.Fatal(errors.Wrap(err, "failed to get pipeline information"))
+                }
+                pipeline = pl
+                if pipeline.Status != "pending" && pipeline.Status != "running" {
+                        break
+                }
+                fmt.Fprintln(w)
+        }
+
+        fmt.Fprintf(w, pipelineStatus(pipeline, jobs))
+        if wait && pipeline.Status != "success" {
+                os.Exit(1)
+        }
+        w.Flush()
+}
+
+func pipelineStatus(pipeline *gitlab.Pipeline, jobs []*gitlab.Job) string {
+        return fmt.Sprintf("\nPipeline Status:\t%s\n%s\n\n%s\n",
+                pipeline.Status, timeMessage(pipeline), jobSummary(jobs))
+}
+
+func jobSummary(jobs []*gitlab.Job) string {
+        numPassed := 0
+        totalJobs := 0
+        numQueued := 0
+        numFailed := 0
+
+        for _, job := range jobs {
+                totalJobs++
+                if (job.Status == "success") {
+                  numPassed++
+                }
+                if (job.Status == "created") {
+                  numQueued++
+                }
+                if (job.Status == "failed") {
+                  numFailed++
+                }
+        }
+
+        return fmt.Sprintf("total\tpassed\tfailed\tqueued\n%d\t%d\t%d\t%d",
+                           totalJobs, numPassed, numFailed, numQueued)
+}
+
+func timeMessage(pipeline *gitlab.Pipeline) string {
+        if pipeline.Status == "pending" {
+          return fmt.Sprintf("created at %v", pipeline.CreatedAt)
+        } else if pipeline.Status == "running" {
+          return fmt.Sprintf("started at %v", pipeline.StartedAt)
+        } else {
+          hours   := pipeline.Duration / (60 * 60)
+          minutes := (pipeline.Duration / 60) - (hours * 60)
+          seconds := pipeline.Duration % 60
+          if hours > 0 {
+            return fmt.Sprintf("duration:\t%d hrs, %d min, %d secs", hours, minutes, seconds)
+          } else if minutes > 0 {
+            return fmt.Sprintf("duration:\t%d min, %d secs", minutes, seconds)
+          } else {
+            return fmt.Sprintf("duration:\t%d secs", pipeline.Duration)
+          }
+        }
 }
 
 func aliasFailures(f *flag.FlagSet, name string) flag.NormalizedName {
@@ -138,6 +205,8 @@ func init() {
 	ciStatusCmd.Flags().BoolVarP(&noSkipped, "no-skipped", "", false, "Ignore skipped tests - do not print them")
 	ciStatusCmd.Flags().BoolVarP(&useColor, "color", "c", false, "Use color for success and failure")
 	ciStatusCmd.Flags().BoolVarP(&onlyFailures, "failures", "f", false, "Only print failures")
+        ciStatusCmd.Flags().BoolVarP(&noCreated, "results-only", "r", false, "Only show completed and running tests. Does not report queued jobs")
+        ciStatusCmd.Flags().BoolVarP(&summaryOnly, "summary", "s", false, "Do not show individual jobs, just the pipeline summary")
         ciStatusCmd.Flags().SetNormalizeFunc(aliasFailures)
 	ciCmd.AddCommand(ciStatusCmd)
 }
