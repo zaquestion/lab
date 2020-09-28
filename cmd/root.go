bb4aeefc9ef227dbcd9d8ad9068e22a2de1b1420
Bruno Meneguele <bmeneg@redhat.com>
2020-09-28 12:29:14 -0300

    root: fix default branch remote discovery
    The default branch (master) remote was never being actually set to
    'forkedFromRemote', since the conditional was not being met in the current
    code state.  This patch change the initialization value for that and also
    changes the order of things being set, so we have a clear precedence order.
    
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- cmd/root.go
+++ cmd/root.go
@@ -253,9 +253,9 @@ func setCommandPrefix(scmd *cobra.Command) {
 
 var (
 	// Will be updated to upstream in Execute() if "upstream" remote exists
-	forkedFromRemote = "origin"
+	forkedFromRemote = ""
 	// Will be updated to lab.User() in Execute() if forkedFrom is "origin"
-	forkRemote = "origin"
+	forkRemote = ""
 )
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -265,21 +265,28 @@ func Execute() {
 	if err == nil {
 		forkedFromRemote = "upstream"
 	}
+	_, err = gitconfig.Local("remote.origin.url")
+	if err == nil {
+		forkedFromRemote = "origin"
+	}
+
 	if forkedFromRemote == "" {
-		// use the remote tracked by the branch if set
-		masterRemote, err := gitconfig.Local("branch.master.remote")
-		if err == nil {
-			forkedFromRemote = masterRemote
+		// use the remote tracked by the default branch if set
+		if remote, err := gitconfig.Local("branch.main.remote"); err == nil {
+			forkedFromRemote = remote
+		} else if remote, err = gitconfig.Local("branch.master.remote"); err == nil {
+			forkedFromRemote = remote
 		}
 	}
 
-	if forkedFromRemote == "origin" {
-		// Check if the user fork exists
-		_, err := gitconfig.Local("remote." + lab.User() + ".url")
-		if err == nil {
-			forkRemote = lab.User()
-		}
+	// Check if the user fork exists
+	_, err = gitconfig.Local("remote." + lab.User() + ".url")
+	if err == nil {
+		forkRemote = lab.User()
+	} else {
+		forkRemote = forkedFromRemote
 	}
+
 	// Check if the user is calling a lab command or if we should passthrough
 	// NOTE: The help command won't be found by Find, which we are counting on
 	cmd, _, err := RootCmd.Find(os.Args[1:])
