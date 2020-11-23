cddff7fa90d6a2fac0b0ac748ab7e896b6721c3e
Prarit Bhargava <prarit@redhat.com>
2020-11-23 11:20:13 -0500

    lab: Add --with-comment option to mr approve and unapprove commands
    Some users would like to add a comment when they approve or unapprove a
    merge request.
    
    Add a --with-comment option to the 'mr approve' and 'mr unapprove'
    commands.
    
    Signed-off-by: Prarit Bhargava <prarit@redhat.com>

--- cmd/mr_unapprove.go
+++ cmd/mr_unapprove.go
@@ -28,6 +28,29 @@ var mrUnapproveCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 
+		comment, err := cmd.Flags().GetBool("with-comment")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if comment {
+			msgs, err := cmd.Flags().GetStringArray("message")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			filename, err := cmd.Flags().GetString("file")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			linebreak, err := cmd.Flags().GetBool("force-linebreak")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			createNote(rn, true, int(id), msgs, filename, linebreak)
+		}
+
 		err = lab.MRUnapprove(p.ID, int(id))
 		if err != nil {
 			log.Fatal(err)
@@ -37,6 +60,10 @@ var mrUnapproveCmd = &cobra.Command{
 }
 
 func init() {
+	mrUnapproveCmd.Flags().Bool("with-comment", false, "Add a comment with the approval")
+	mrUnapproveCmd.Flags().StringArrayP("message", "m", []string{}, "use the given <msg>; multiple -m are concatenated as separate paragraphs (used with --with-comment only)")
+	mrUnapproveCmd.Flags().StringP("file", "F", "", "use the given file as the message (used with --with-comment only)")
+	mrUnapproveCmd.Flags().Bool("force-linebreak", false, "append 2 spaces to the end of each line to force markdown linebreaks (used with --with-comment only)")
 	mrCmd.AddCommand(mrUnapproveCmd)
 	carapace.Gen(mrUnapproveCmd).PositionalCompletion(
 		action.Remotes(),
