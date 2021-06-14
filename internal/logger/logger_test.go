b1c4a743f218e1c4b908b92346d4c1a2667b27ed
Bruno Meneguele <bmeneg@redhat.com>
2021-06-14 16:37:52 -0300

    internal/logger_test: add test for all logging functions
    The logging functions didn't have a proper test code in place. This patch
    adds that to make sure the format isn't broken by future changes.
    
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -1,6 +1,9 @@
 package logger
 
 import (
+	"bytes"
+	"io"
+	"os"
 	"regexp"
 	"testing"
 
@@ -36,4 +39,83 @@ func Test_addFileLinePrefix(t *testing.T) {
 	msg := addFileLinePrefix("test")
 	regex := regexp.MustCompile("testing.go:[0-9]+: test")
 	require.Regexp(t, regex, msg)
+}
+
+func TestLogFunctions(t *testing.T) {
+	type logFunc func(string, ...interface{})
+	type logFuncf func(string, ...interface{})
+	type logFuncln func(...interface{})
+
+	log := GetInstance()
+	log.SetLogLevel(LOG_DEBUG)
+
+	tests := []struct {
+		name   string
+		prefix string
+		fn     logFunc
+		fnf    logFuncf
+		fnln   logFuncln
+	}{
+		{
+			name:   "error",
+			prefix: "ERROR:",
+			fn:     log.Error,
+			fnf:    log.Errorf,
+			fnln:   log.Errorln,
+		},
+		{
+			name:   "warn",
+			prefix: "WARNING:",
+			fn:     log.Warn,
+			fnf:    log.Warnf,
+			fnln:   log.Warnln,
+		},
+		{
+			name:   "info",
+			prefix: "",
+			fn:     log.Info,
+			fnf:    log.Infof,
+			fnln:   log.Infoln,
+		},
+		{
+			name:   "debug",
+			prefix: "DEBUG:",
+			fn:     log.Debug,
+			fnf:    log.Debugf,
+			fnln:   log.Debugln,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Redirect system stdout to our own so we can check log output
+			// to stdout
+			oldStdout := os.Stdout
+			r, w, err := os.Pipe()
+			if err != nil {
+				t.Errorf("failed to redirect stdout: %s", err)
+			}
+			os.Stdout = w
+			log.SetStdDest(w, w)
+			outChan := make(chan string)
+
+			test.fn("test")
+			test.fnf("test\n")
+			test.fnln("test")
+
+			go func() {
+				var buf bytes.Buffer
+				io.Copy(&buf, r)
+				outChan <- buf.String()
+			}()
+
+			w.Close()
+			os.Stdout = oldStdout
+			out := <-outChan
+
+			regex := regexp.MustCompile(test.prefix + " logger_test.go:[0-9]+: test")
+			require.Regexp(t, regex, out)
+		})
+	}
+
 }
