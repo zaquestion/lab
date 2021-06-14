0e02d27edf61fcb8b93526dc45720a256429c742
Bruno Meneguele <bmeneg@redhat.com>
2021-06-14 16:35:36 -0300

    internal/logger: fix keysAndValues overflow
    The limit should not have the "equal" part, since it moves in pairs and the
    last element should be n-1 instead of n.
    
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -80,7 +80,7 @@ func (l *logger) SetStdDest(stdout io.Writer, stderr io.Writer) {
 // printKeysAndValues prints the keys and valus, as pairs, passed to those
 // functions in the way expected by go-retryablehttp LeveledLogger interface
 func printKeysAndValues(l *log.Logger, keysAndValues ...interface{}) {
-	for i := 0; i <= len(keysAndValues)/2; i += 2 {
+	for i := 0; i < len(keysAndValues)/2; i += 2 {
 		l.Printf("\t%s = %s\n", keysAndValues[i], keysAndValues[i+1])
 	}
 }
