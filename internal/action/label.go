51696f9164fd592bd462e40b6ff11a9dfc43a7a3
Bruno Meneguele <bmeneg@redhat.com>
2021-06-22 21:26:58 -0300

    internal/action: fix else clause with if ending with return
    If the 'if' clause ends with a 'return' call the 'else' can be removed.
    
    Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

--- internal/action/label.go
+++ internal/action/label.go
@@ -11,15 +11,16 @@ import (
 // Labels returns a carapace.Action containing all possible labels
 func Labels(project string) carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
-		if labels, err := lab.LabelList(project); err != nil {
+		labels, err := lab.LabelList(project)
+		if err != nil {
 			return carapace.ActionMessage(err.Error())
-		} else {
-			values := make([]string, len(labels)*2)
-			for index, label := range labels {
-				values[index*2] = label.Name
-				values[index*2+1] = label.Description
-			}
-			return carapace.ActionValuesDescribed(values...)
 		}
+
+		values := make([]string, len(labels)*2)
+		for index, label := range labels {
+			values[index*2] = label.Name
+			values[index*2+1] = label.Description
+		}
+		return carapace.ActionValuesDescribed(values...)
 	}).Cache(5*time.Minute, cache.String(project))
 }
--- internal/action/milestone.go
+++ internal/action/milestone.go
@@ -19,9 +19,8 @@ type MilestoneOpts struct {
 func (o MilestoneOpts) format() string {
 	if o.Active {
 		return "active"
-	} else {
-		return "closed"
 	}
+	return "closed"
 }
 
 // Milestones returns a carapace.Action containing all possible milestones with
@@ -29,15 +28,16 @@ func (o MilestoneOpts) format() string {
 func Milestones(project string, opts MilestoneOpts) carapace.Action {
 	return carapace.ActionCallback(func(c carapace.Context) carapace.Action {
 		state := opts.format()
-		if milestones, err := lab.MilestoneList(project, &gitlab.ListMilestonesOptions{State: &state}); err != nil {
+		milestones, err := lab.MilestoneList(project, &gitlab.ListMilestonesOptions{State: &state})
+		if err != nil {
 			return carapace.ActionMessage(err.Error())
-		} else {
-			values := make([]string, len(milestones)*2)
-			for index, milestone := range milestones {
-				values[index*2] = milestone.Title
-				values[index*2+1] = strings.SplitN(milestone.Description, "\n", 2)[0]
-			}
-			return carapace.ActionValuesDescribed(values...)
 		}
+
+		values := make([]string, len(milestones)*2)
+		for index, milestone := range milestones {
+			values[index*2] = milestone.Title
+			values[index*2+1] = strings.SplitN(milestone.Description, "\n", 2)[0]
+		}
+		return carapace.ActionValuesDescribed(values...)
 	}).Cache(5*time.Minute, cache.String(project, opts.format()))
 }
