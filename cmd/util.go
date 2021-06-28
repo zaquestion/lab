zaquestion/lab

commit ecd57aaf4afa4f055ac2209e4788ec2c9787099a

cmd/util: avoid an empty string to be parsed
An additional empty string was being added in the array of "branchArgs" to
later be processed as a valid branch/remote information by the argument
parses. It wasn't an issue before because a "err" return value was being
ignored somewhere else within the parses, but the commit 69ae19e14cd2 ("cmd:
fix return value checks and statements") changed it and the error finally
was cought.

Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

cmd/util.go
@@ -345,6 +345,8 @@ func parseArgsWithGitBranchMR(args []string) (string, int64, error) {
 	return s, i, nil
 }
 
+// filterCommentArg separate the case where a command can have both the
+// remote and "<mrID>:<commentID>" at the same time.
 func filterCommentArg(args []string) (int, []string, error) {
 	branchArgs := []string{}
 	idString := ""
@@ -359,16 +361,18 @@ func filterCommentArg(args []string) (int, []string, error) {
 		} else {
 			idString = args[0]
 		}
-	} else if len(args) > 1 {
+	} else if len(args) == 2 {
 		branchArgs = append(branchArgs, args[0])
 		idString = args[1]
+	} else {
+		return 0, branchArgs, fmt.Errorf("unsupported number of arguments")
 	}
 
 	if strings.Contains(idString, ":") {
 		ps := strings.Split(idString, ":")
 		branchArgs = append(branchArgs, ps[0])
 		idString = ps[1]
-	} else {
+	} else if idString != "" {
 		branchArgs = append(branchArgs, idString)
 		idString = ""
 	}

commit d4c4280ce1d6efb11a77c38e1ebde013197c9cbd

cmd/util: fix handle of unknown number of arguments
Instead of returning an error in case of an unknown number of args, let
it return the nil values and let the caller handle it as it please. For
"mr_note", "issue_note" and "mr_edit" the error was interrumpting the
cmd line argument parser.

Fixes: ecd57aaf4afa ("cmd/util: avoid an empty string to be parsed")
Signed-off-by: Bruno Meneguele <bmeneg@redhat.com>

cmd/util.go
@@ -357,8 +357,6 @@ func filterCommentArg(args []string) (int, []string, error) {
 	} else if len(args) == 2 {
 		branchArgs = append(branchArgs, args[0])
 		idString = args[1]
-	} else {
-		return 0, branchArgs, fmt.Errorf("unsupported number of arguments")
 	}
 
 	if strings.Contains(idString, ":") {