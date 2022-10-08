c715d45dec4be5b00aa5258aff093f706c713232
Clayton Carter <claytonrcarter@gmail.com>
2022-10-07 22:53:12 -0400

    feat(issue/mr list): Support `--assignee=any/none`

--- cmd/mr_list_test.go
+++ cmd/mr_list_test.go
@@ -23,8 +23,44 @@ func Test_mrListAssignedTo(t *testing.T) {
 	mrs := strings.Split(string(b), "\n")
 	t.Log(mrs)
 	require.Contains(t, mrs, "!1 Test MR for lab list")
-	require.NotContains(t, mrs, "!3")
-	require.NotContains(t, mrs, "filtering with labels and lists")
+	require.NotContains(t, mrs, "!329 MR for assign and review commands")
+	require.NotContains(t, mrs, "!3 for testings filtering with labels and lists")
+}
+
+func Test_mrListAssignedToAny(t *testing.T) {
+	t.Parallel()
+	repo := copyTestRepo(t)
+	cmd := exec.Command(labBinaryPath, "mr", "list", "--assignee=any")
+	cmd.Dir = repo
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mrs := strings.Split(string(b), "\n")
+	t.Log(mrs)
+	require.Contains(t, mrs, "!1 Test MR for lab list")
+	require.Contains(t, mrs, "!329 MR for assign and review commands")
+	require.NotContains(t, mrs, "!3 for testings filtering with labels and lists")
+}
+
+func Test_mrListAssignedToNone(t *testing.T) {
+	t.Parallel()
+	repo := copyTestRepo(t)
+	cmd := exec.Command(labBinaryPath, "mr", "list", "--assignee=none")
+	cmd.Dir = repo
+
+	b, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mrs := strings.Split(string(b), "\n")
+	t.Log(mrs)
+	require.NotContains(t, mrs, "!1 Test MR for lab list")
+	require.NotContains(t, mrs, "!329 MR for assign and review commands")
+	require.Contains(t, mrs, "!3 for testings filtering with labels and lists")
 }
 
 func Test_mrList(t *testing.T) {
