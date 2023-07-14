9a8d0d2cc11051e9ef5fe57e295e24966a2c293f
Shang Ding <sding3@ncsu.edu>
2023-07-14 11:07:21 -0500

    remove --branch on project browse command

--- cmd/project_browse_test.go
+++ cmd/project_browse_test.go
@@ -11,7 +11,7 @@ func Test_projectBrowse(t *testing.T) {
 	defer func() { browse = oldBrowse }()
 
 	browse = func(url string) error {
-		require.Equal(t, "https://gitlab.com/zaquestion/test/-/tree/master", url)
+		require.Equal(t, "https://gitlab.com/zaquestion/test/-/blob/master", url)
 		return nil
 	}
 
@@ -19,21 +19,21 @@ func Test_projectBrowse(t *testing.T) {
 }
 
 func Test_projectGetPath(t *testing.T) {
-	defaultPath := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "", "", "")
-	require.Equal(t, defaultPath, "https://gitlab.com/zaquestion/test/-/tree/master")
+	defaultPath := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "", "")
+	require.Equal(t, defaultPath, "https://gitlab.com/zaquestion/test/-/blob/master")
 }
 
 func Test_projectGetPathAndFile(t *testing.T) {
-	pathAndFile := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "", "README.md", "")
+	pathAndFile := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "", "README.md")
 	require.Equal(t, pathAndFile, "https://gitlab.com/zaquestion/test/-/blob/master/README.md")
 }
 
 func Test_projectGetPathAndFileAndBranch(t *testing.T) {
-	pathAndFileAndBranch := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "newbranch", "README.md", "")
-	require.Equal(t, pathAndFileAndBranch, "https://gitlab.com/zaquestion/test/-/blob/newbranch/README.md")
+	pathAndFileAndBranch := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "master", "new/branch", "README.md")
+	require.Equal(t, pathAndFileAndBranch, "https://gitlab.com/zaquestion/test/-/blob/new/branch/README.md")
 }
 
 func Test_projectGetPathAndRef(t *testing.T) {
-	pathRef := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "", "", "", "12345abcdef")
-	require.Equal(t, pathRef, "https://gitlab.com/zaquestion/test/-/tree/12345abcdef")
+	pathRef := projectBrowseGetPath("https://gitlab.com/zaquestion/test", "", "12345abcdef", "")
+	require.Equal(t, pathRef, "https://gitlab.com/zaquestion/test/-/blob/12345abcdef")
 }
